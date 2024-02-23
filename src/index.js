82916721a29cf16c7bf834e250514ba7cd47fe67
Leki <lekisintole82@gmail.com>
2024-02-23 11:24:09 +0300

    post request from client

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ const express = require( 'express' );
 
 const PORT= 3000
 const app= express()
+app.use( express.json())
+
+const groceries = [
+    {name:'apple'},
+    {name: 'banana'}
+]
 app.get('/groceries', (req,res)=>{
-    res.send([
-        {name:'apple'},
-        {name: 'banana'}
-    ])
+    res.send(
+        groceries
+    )
 })
+app.post( '/groceries', ( req, res )=>{
+    const item = req.body;
+    console.log(item)
+    res.send(201)
+})
+
+
+
 app.listen(PORT, ()=> console.log(`App running on serverListening on ${PORT}`))
+
