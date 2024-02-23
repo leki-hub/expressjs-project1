f03113ac8647c978b76452b2714c9fc42d555dc9
Leki <lekisintole82@gmail.com>
2024-02-23 10:49:54 +0300

    server created

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 const express = require( 'express' );
-const router = express.Router();
+
+const PORT= 3000
+const app= express()
+app.listen(PORT, ()=> console.log(`App running on serverListening on ${PORT}`))
