729ec8ce55fdac14744f4189f3ac7c0c9ac405f7
Leki <lekisintole82@gmail.com>
2024-02-26 12:33:40 +0300

    cookie parser use

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 const express = require( 'express' );
 const grocerieRoutes = require( '../routes/groceries.js');
 const marketroutes= require( '../routes/markets.js') ; 
-
+const cookieParser = require("cookie-parser")
 const PORT= 3000
 const app= express()
 app.use( express.json())
 app.use( express.urlencoded({ extended: true }))
+app.use(cookieParser());
 // set a simple middleware usable to all routes in the application
 app.use((req,res,next)=>{
     console.log(req.url)
