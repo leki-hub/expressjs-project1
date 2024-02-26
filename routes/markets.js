78a9ea84c28252e5f05623af96705002aec5367b
Leki <lekisintole82@gmail.com>
2024-02-26 11:17:30 +0300

    query/filtered items

--- routes/markets.js
+++ routes/markets.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const{Router} = require(('express'));
 const marketrouter= Router();
 const  supermarkets=[
@@ -22,6 +24,13 @@ const  supermarkets=[
 ];
 marketrouter.get('/', (request,response)=>{
     console.log(request.query)
+    const {miles} = request.query;
+    const parsedMile= parseInt(miles);
+    const filteredStores= supermarkets.filter((supermarket)=>supermarket.miles <=parsedMile);
+    if(!isNaN(parsedMile)){
+      response.send(filteredStores)
+    }
+    else
     response.send(supermarkets);
 })
 
