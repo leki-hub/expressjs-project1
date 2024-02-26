f1fc66e4b244b30ff8d9ee3efdfd356b7e138c81
Leki <lekisintole82@gmail.com>
2024-02-26 23:49:17 +0300

    session

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ const express = require( 'express' );
 const grocerieRoutes = require( '../routes/groceries.js');
 const marketroutes= require( '../routes/markets.js') ; 
 const cookieParser = require("cookie-parser")
+const sessionMiddleware  = require('express-session')
 const PORT= 3000
 const app= express()
 app.use( express.json())
 app.use( express.urlencoded({ extended: true }))
 app.use(cookieParser());
+app.use(sessionMiddleware(
+    {
+        secret:"secret",
+        resave: false,
+        saveUninitialized :false
+        
+    }
+))
 // set a simple middleware usable to all routes in the application
 app.use((req,res,next)=>{
     console.log(req.url)
