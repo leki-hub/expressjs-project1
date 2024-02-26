02aaa574e08a892f20c256f68737285ebb534a3c
Leki <lekisintole82@gmail.com>
2024-02-26 09:55:32 +0300

    clean route code

--- routes/groceries.js
+++ routes/groceries.js
@@ -10,19 +10,19 @@ const groceries = [
     {item: 'orange',quantity:10}
 ]
 
-router.get('/groceries', (req,res)=>{
+router.get('', (req,res)=>{
     res.send(
         groceries
     )
 })
-router.post( '/groceries', ( req, res )=>{
+router.post( '', ( req, res )=>{
     const item = req.body;
     groceries.push(item)
     console.log(item)
     res.send(201, "data created")
 })
 
-router.get('/groceries/:item', (req,res)=>{
+router.get('/:item', (req,res)=>{
     const {item} = req.params
     const groceryItem= groceries.find(g => g.item ===item)
    res.send(groceryItem)
