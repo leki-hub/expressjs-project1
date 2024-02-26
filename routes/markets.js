9a0a8f688e3f0c04f2b2b79fb241f538f60d2df0
Leki <lekisintole82@gmail.com>
2024-02-26 10:46:00 +0300

    more market props

--- routes/markets.js
+++ routes/markets.js
@@ -1,9 +1,24 @@
 const{Router} = require(('express'));
 const marketrouter= Router();
 const  supermarkets=[
-    {store:"Whole foods"},
-    {store:"Trader Joe's"},
-    {store:"Costco"}
+    {id:1,
+        store:"Whole foods",
+    miles:2},
+    {id:2,
+        store:"John Kim",
+    miles:2.5},
+    {id:3,
+        store:"Whole foods",
+    miles:3},
+    {id:1,
+        store:"Whole foods",
+    miles:2},
+    {id:2,
+        store:"John Kim",
+    miles:2.5},
+    {id:3,
+        store:"Whole foods",
+    miles:3}
 ];
 marketrouter.get('/', (req,res)=>{
     res.send(supermarkets);
