2b292521182a7bf5c9d36b77a5112eb0c0478c46
Leki <lekisintole82@gmail.com>
2024-02-26 09:10:39 +0300

    routes api set up

--- routes/groceries.js
+++ routes/groceries.js
@@ -1,3 +1,22 @@
 const {Router} = require(express)
 
-const  router = Router()
+const  router = Router();
+
+router.get('/groceries', (req,res)=>{
+    res.send(
+        groceries
+    )
+})
+router.post( '/groceries', ( req, res )=>{
+    const item = req.body;
+    groceries.push(item)
+    console.log(item)
+    res.send(201, "data created")
+})
+
+router.get('/groceries/:item', (req,res)=>{
+    const {item} = req.params
+    const groceryItem= groceries.find(g => g.item ===item)
+   res.send(groceryItem)
+})
+
