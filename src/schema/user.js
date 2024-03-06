d37f7f16772d36552108412ea5a9befdde13afe3
Leki <lekisintole82@gmail.com>
2024-03-06 11:37:37 +0300

    schema set up

--- src/schema/user.js
+++ src/schema/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const userSchema= new mongoose.Schema({
-    name: { type: String, required: true },
-    email: {
-        type: String, 
-        unique: true, }
+    username: { type: mongoose.SchemaTypes.String, required: true },
+    password: { type: mongoose.SchemaType.String,  unique: true, },
+    email: { type: mongoose.SchemaTypes.String, required: true },
 })
