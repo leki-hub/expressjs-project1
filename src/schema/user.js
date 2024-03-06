322ec2feb77884c80f660e687d32e9693f98372a
Leki <lekisintole82@gmail.com>
2024-03-06 11:43:25 +0300

    other schema props

--- src/schema/user.js
+++ src/schema/user.js
@@ -3,4 +3,9 @@ const userSchema= new mongoose.Schema({
     username: { type: mongoose.SchemaTypes.String, required: true },
     password: { type: mongoose.SchemaType.String,  unique: true, },
     email: { type: mongoose.SchemaTypes.String, required: true },
+    createdAt:{
+        type : mongoose.SchemaTypes.Date ,
+        required:true,
+        default: new Date.now()
+    }
 })
