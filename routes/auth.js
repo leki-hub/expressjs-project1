2d6ec003bcfc17a75bf918635c5dcc1112b442f1
Leki <lekisintole82@gmail.com>
2024-03-06 12:22:19 +0300

    authorization end points

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const User = require('./schemas/user');
+
+const router = Router();
+
+router.post('/login', (request, response) => {
+  const { username, password } = request.body;
+  if (username && password) {
+    if (request.session.user) {
+      response.send(request.session.user);
+    } else {
+      request.session.user = {
+        username,
+      };
+      response.send(request.session);
+    }
+  } else response.send(401);
+});
+
+router.post('/register', async (request, response) => {
+  const { username, password, email } = request.body;
+  const userDB = await User.findOne({ $or: [{ username }, { email }] });
+  if (userDB) {
+    response.status(400).send({ msg: 'User already exists!' });
+  } else {
+    const newUser = await User.create({ username, password, email });
+    response.send(201);
+  }
+});
+
+module.exports = router;
