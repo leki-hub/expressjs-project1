e722973219e5bb22bfd9bbb0de7a3a5e32754fb1
Leki <lekisintole82@gmail.com>
2024-03-08 11:04:44 +0300

    email to be unique

--- src/schema/user.js
+++ src/schema/user.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const userSchema= new mongoose.Schema({
-    username: { type: mongoose.SchemaTypes.String, required: true },
-    password: { type: mongoose.SchemaType.String,  unique: true, },
-    email: { type: mongoose.SchemaTypes.String, required: true },
+    // username: { type: mongoose.SchemaTypes.String, required: true },
+    password: { type: mongoose.SchemaTypes.String,  unique: true, },
+    email: { type: mongoose.SchemaTypes.String, required: true , unique:true},
     createdAt:{
         type : mongoose.SchemaTypes.Date ,
         required:true,
-        default: new Date.now()
+        default: new Date()
     }
 })
 
