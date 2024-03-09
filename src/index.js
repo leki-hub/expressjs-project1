6914b0afc9689dc4bafb27c6aafb5d5b5f83c190
Leki <lekisintole82@gmail.com>
2024-03-09 23:46:16 +0300

    session secret value

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use( express.urlencoded({ extended: true }))
 app.use(cookieParser());
 app.use(session(
     {
-        secret:"secret",
+        secret:"0hdd03uns008$890dkff0nsgd",
         resave: false,
         saveUninitialized :false
         
