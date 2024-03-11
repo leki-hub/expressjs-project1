fff80a95ce918e043b1ffa4ab9b3975be9d3c2a4
Leki <lekisintole82@gmail.com>
2024-03-11 11:31:49 +0300

    verify login from db

--- src/strategies/local.js
+++ src/strategies/local.js
@@ -1,9 +1,15 @@
 const passport =  require('passport');
+const User = require( './strategies/schema/user' );
 const {Strategy} = require('passport-local')
 passport.use(new Strategy(
     {usernameField: 'email'}, // use the email field for username
-    (username, password, done)=> {
-    // console.log("User login: ",
-    
+  async  (username, password, done)=> {
+    if(!username || !password) {
+       throw  new Error("Username and Password are required")
+    }
+const userDB= await  User.findOne({email });
+if (!userDB){
+    return done(null , false ,{message : "No user found"})
+}
 
 }))
