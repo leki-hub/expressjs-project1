f3b2110822bc630f39561579813544013453ed65
Leki <lekisintole82@gmail.com>
2024-03-11 11:56:23 +0300

    passport set up

--- src/strategies/local.js
+++ src/strategies/local.js
@@ -1,15 +1,26 @@
 const passport =  require('passport');
-const User = require( './strategies/schema/user' );
+const User = require( '../schema/user' );
 const {Strategy} = require('passport-local')
+const  {comparePasswords} = require('../utils/Helpers')
 passport.use(new Strategy(
     {usernameField: 'email'}, // use the email field for username
   async  (username, password, done)=> {
     if(!username || !password) {
        throw  new Error("Username and Password are required")
     }
-const userDB= await  User.findOne({email });
+const userDB= await  User.findOne({email:username });
 if (!userDB){
     return done(null , false ,{message : "No user found"})
 }
+const isValidated = comparePasswords(password, userDB.password);
+if(isValidated){
+console.log('User validated,  Logged In Successfully!')
+done(null , userDB )
+}
+else {
+  console.log('Invalid email or  Password')
+done(null,null)
+}
+
 
 }))
